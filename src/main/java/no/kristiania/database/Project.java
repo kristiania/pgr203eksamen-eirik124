52b299a7ce70bb316d7c3a0d486a8b058bea3403
eirik124 <eirik.lundanes@gmail.com>
2020-10-23 13:24:45 +0200

    #3 Project DAO should list all Projects (test running red)

---
+++ src/main/java/no/kristiania/database/Project.java
@@ -0,0 +1,13 @@
+package no.kristiania.database;
+
+public class Project {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/no/kristiania/database/ProjectDaoTest.java
@@ -0,0 +1,39 @@
+package no.kristiania.database;
+
+import org.flywaydb.core.Flyway;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class ProjectDaoTest {
+    private ProjectDao projectDao;
+
+    @BeforeEach
+    void setUp() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        Flyway.configure().dataSource(dataSource).load().migrate();
+        projectDao = new ProjectDao(dataSource);
+    }
+
+    @Test
+    void shouldListAllProjects() {
+        Project project1 = exampleProject();
+        Project project2 = exampleProject();
+        assertThat(projectDao.list())
+                .extracting(Project::getName)
+                .contains(project1.getName(), project2.getName());
+
+    }
+
+    private Project exampleProject() {
+        return null;
+    }
+
+    @Test
+    void shouldRetrieveAllProjectProperties() {
+
+    }
+}
